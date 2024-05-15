2792e11b1d708dcda5733a4933dd578f3b4eedf5
Manolotsoa Daniel Razafindrakoto <manolotsoadaniel@gmail.com>
2024-05-15 08:28:26 +0300

    add cors permission

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ let port = process.env.PORT || 8010;
 const { main } = require('./routes/main');
 const { sendGridRoutes } = require('./routes/sendgrid.routes');
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 // les routes
 const prefix = '/api'
 
